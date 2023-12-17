13fc4284fe6fd4d6710018104cacd2238bb24fc7
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2023-12-17 10:42:48 +0100

    Subiendo updateCompany y deleteCompany
    Subiendo updateCompany y deleteCompany

--- src/main/java/com/jacaranda/EmployeeProjectSpring/controller/CompanyController.java
+++ src/main/java/com/jacaranda/EmployeeProjectSpring/controller/CompanyController.java
@@ -1,13 +1,16 @@
 package com.jacaranda.EmployeeProjectSpring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jacaranda.EmployeeProjectSpring.model.Company;
 import com.jacaranda.EmployeeProjectSpring.service.CompanyService;
@@ -45,6 +48,48 @@ public class CompanyController {
 	    // Redirige a la URL "/listCompanies"
 	    return "redirect:/listCompanies";
 	}
+	
+	@GetMapping("/updateCompany")
+	public String updateCompany(Model model, @RequestParam("id") int companyId) {
+	    Company company = companyService.findIdCompany(companyId);
+	    model.addAttribute("company", company);
+	    return "updateCompany";
+	}
 
 
+	@PostMapping("/updateCompany/updated")
+	public String updatedCompany(@ModelAttribute("company") Company updatedCompany) {
+		Company companyId = companyService.findIdCompany(updatedCompany.getId());
+		
+		if(companyId != null) {
+			companyService.updateCompany(updatedCompany);
+			return "redirect:/listCompanies";
+		} else {
+			return "updateCompany";
+		}
+	}
+
+	
+	@GetMapping("/deleteCompany")
+	public String deleteCompany(Model model, @RequestParam("id") int companyId) {
+	    
+	    Company company = companyService.findIdCompany(companyId);
+	    model.addAttribute("company", company);
+	    return "deleteCompany";
+	}
+	
+	@PostMapping("/deleteCompany/deleted")
+	public String deletedCompany(@ModelAttribute("company") Company deleteCompany) {
+		Company companyId = companyService.findIdCompany(deleteCompany.getId());
+		
+		if(companyId != null) {
+			companyService.deleteCompany(deleteCompany);
+			return "redirect:/listCompanies";
+		} else {
+			return "deleteCompany";
+		}
+	}
+
+
+	
 }
--- src/main/java/com/jacaranda/EmployeeProjectSpring/service/CompanyService.java
+++ src/main/java/com/jacaranda/EmployeeProjectSpring/service/CompanyService.java
@@ -1,6 +1,7 @@
 package com.jacaranda.EmployeeProjectSpring.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,25 @@ public class CompanyService {
 	public Company addCompany(Company c) {
 		return companyRepository.save(c);
 	}
+	
+	public Company findIdCompany(Integer id) {
+		return companyRepository.findById(id).orElse(null);
+		
+	}
+	
+	public Company updateCompany(Company updatedCompany) {
+		return companyRepository.save(updatedCompany);
+		
+	}
+	
+	public void deleteCompany(Company deleteCompany) {
+
+	    	companyRepository.delete(deleteCompany);
+	    
+	}
+
+
+
+	
 
 }
